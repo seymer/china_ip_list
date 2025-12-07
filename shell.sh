8553f61a100997ab36e54695ea30a17587e6a940
Seymer <siqingmu@gmail.com>
2025-12-07 19:53:34 +0800

    bug-fixes

--- shell.sh
+++ shell.sh
@@ -6,8 +6,6 @@ echo "/log info \"Start removing old list!\""
 echo "/ip firewall address-list remove [/ip firewall address-list find list=CN]"
 echo "/log info \"Remove finished!\""
 echo "/log info \"Start importing CN_IP_List!\""
-echo "/ip firewall address-list"
-echo "add list=CN address=192.168.0.0/16"
 nets=`cat ./china_ip_list.txt`
 for net in $nets ; do
   echo "add list=CN address=$net "
